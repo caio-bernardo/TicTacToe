4f2995c0446b88496aa281c29088c03982a2593f
Caio Bernardo <caio.2004vb@gmail.com>
2022-01-23 17:37:03 -0300

    Create app.py
    Arquivo principal app.py criado.

---
+++ app.py
@@ -0,0 +1,12 @@
+"""
+
+- Create a landing page containing selection buttons: Single-player or 
+   multiplayer.
+- Create a game-board containing nine tiles to play the game along with other 
+   details (i.e. playing with a system or another player, whose turn etc.).
+- Allow the player to press the tile and check the status of the game (i.e. Tie
+   game, anyone of the players won the match or the game is still running).
+- Display the message, who won the match.
+"""
+
+import tkinter
